ab726315a0bda8166c36849ccb6ef73149caf7e4
J Dileep Kumar <jdileepkumar1@gmail.com>
2026-04-20 15:57:02 +0530

    Add files via upload

---
+++ create new node.cpp
@@ -0,0 +1,22 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+struct node
+{
+    int data;
+    struct node *next;
+};
+
+int main()
+{
+    struct node *newnode;
+
+    newnode = (struct node*)malloc(sizeof(struct node)); 
+
+    newnode->data = 10;
+    newnode->next = NULL;
+
+    printf("%d", newnode->data);
+
+    return 0;
+}
